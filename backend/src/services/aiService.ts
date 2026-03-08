0140dc37248e304f32fdbc6e0c4eb3b982133c70
Daniel <danieldetrabalho@hotmail.com>
2026-03-08 02:34:27 -0300

    refactor: melhorando a AI e reduzindo os tokens e colocando cache na chamada

--- backend/src/services/aiService.ts
+++ backend/src/services/aiService.ts
@@ -1,6 +1,54 @@
 import { GoogleGenAI, Type } from "@google/genai";
 import { Transacao, AnaliseIA } from "../types";
 
+import crypto from "crypto";
+import db from "@/config/db";
+
+// Refatorar
+function gerarHash(dados: any) {
+  return crypto.createHash("sha256").update(JSON.stringify(dados)).digest("hex");
+}
+
+function gerarResumoFinanceiro(transacoes: any[], salario: number) {
+
+  let totalReceita = 0;
+  let totalDespesa = 0;
+  const categorias: Record<string, number> = {};
+
+  for (const t of transacoes) {
+
+    if (t.tipo === "RECEITA") {
+      totalReceita += Number(t.valor);
+    } else {
+      totalDespesa += Number(t.valor);
+    }
+
+    if (!categorias[t.categoria]) {
+      categorias[t.categoria] = 0;
+    }
+
+    categorias[t.categoria] += Number(t.valor);
+  }
+
+  const saldo = totalReceita - totalDespesa;
+
+  const maiorGasto = Object.entries(categorias)
+    .sort((a, b) => b[1] - a[1])[0]?.[0] || null;
+
+  const percentualPoupanca =
+    salario > 0 ? ((saldo / salario) * 100).toFixed(1) : 0;
+
+  return {
+    salario,
+    totalReceita,
+    totalDespesa,
+    saldo,
+    categorias,
+    maiorGasto,
+    percentualPoupanca
+  };
+}
+
 export class AIService {
   private static ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY || '' });
 
@@ -9,26 +57,59 @@ export class AIService {
       throw new Error("Chave de api não configurada.");
     }
 
-    const dadosFinanceiros = JSON.stringify({
-      salario_mensal: salario,
-      historico_transacoes: transacoes.map(t => ({
-        ...t,
-        valor: t.tipo === 'DESPESA' ? -t.valor : t.valor
-      }))
-    })
+    const ultimasTransacoes = transacoes.slice(-30).map(t => [
+      t.tipo === 'DESPESA' ? 'D' : 'R',
+      t.tipo === 'DESPESA' ? -t.valor : t.valor,
+      t.categoria
+    ]);
 
-    const prompt = `Atue como um Consultor Financeiro Sênior e Auditor. Analise os seguintes dados financeiros brutos.
-      Identifique padrões de gastos, desperdícios, e sugira otimizações agressivas para retorno financeiro.
-      Calcule potenciais de investimento se o usuário seguir as dicas.
-      
-      Dados: ${dadosFinanceiros}
-    `;
+
+    const resumoBase = gerarResumoFinanceiro(transacoes, salario);
+
+    const resumo = {
+      s: resumoBase.salario,
+      sd: resumoBase.saldo,
+      tp: resumoBase.percentualPoupanca,
+      mg: resumoBase.maiorGasto,
+      r: resumoBase.saldo < 0 ? "ALTO" : "BAIXO",
+      c: Object.entries(resumoBase.categorias)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 3)
+    };
+    const hash = gerarHash(resumo);
+
+
+    const result = await db.query("SELECT resultado FROM analise_cache WHERE hash = $1", [hash])
+
+    if (result.rows.length > 0) {
+      return JSON.parse(result.rows[0].resultado);
+    }
+
+
+    const prompt = `Analise a saúde financeira do usuário.
+      Dados resumidos: ${JSON.stringify(resumo)}
+
+      Retorne:
+      - auditoria
+      - sugestões práticas
+      - projeção de investimento
+      - risco financeiro
+     `;
 
     try {
       const response = await this.ai.models.generateContent({
         model: 'gemini-3-flash-preview',
         contents: prompt,
         config: {
+          topK: 1,
+          topP: 0.8,
+          temperature: 0.1,
+          maxOutputTokens: 290,
+          thinkingConfig: {
+            includeThoughts: false,
+            thinkingBudget: 0,
+          },
+
           systemInstruction: "Você é um especialista em finanças pessoais focado em acumulação de riqueza e eficiência. Seja direto e analítico.",
           responseMimeType: "application/json",
           responseSchema: {
@@ -62,7 +143,9 @@ export class AIService {
         throw new Error("Resposta da IA vazia.");
       }
 
-      return JSON.parse(response.text) as AnaliseIA;
+      const resultadoIa = JSON.parse(response.text) as AnaliseIA;
+      await db.query(`INSERT INTO analise_cache (hash, resultado) VALUES ($1, $2) ON CONFLICT (hash) DO NOTHING`, [hash, response.text]);
+      return resultadoIa;
 
     } catch (error) {
       console.error("Erro no AIService:", error);
